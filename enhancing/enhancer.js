42651285562be57a1ddf67ec70f435aa350f5a2c
shaunmcarmody <shaun.carmody@live.co.uk>
2019-04-08 23:08:36 +0100

    test fail and suceed methods

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,10 +6,26 @@ module.exports = {
 };
 
 function succeed(item) {
+  if (item === null || typeof item !== 'object')  return undefined;
+  if (!Object.keys(item).includes('enhancement')) return undefined;
+  if (item.enhancement < 20) {
+    item.enhancement += 1
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item === null || typeof item !== 'object')  return undefined;
+  const props = Object.keys(item);
+  if (!props.includes('enhancement') || !props.includes('durability')) return undefined;
+  if (item.enhancement < 15) {
+    item.durability -= 5;
+  } else if (item.enhancement >= 15) {
+    item.durability -= 10;
+  }
+  if (item.enhancement > 16) {
+    item.enhancement -= 1;
+  }
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -4,7 +4,7 @@ const enhancer = require('./enhancer.js');
 describe('enhancer.js', () => {
   const item = {
     durability: 0,
-    enhacement: 50,
+    enhancement: 14,
     name: 'Player 1'
   }
   describe('repair method', () => {
@@ -24,7 +24,63 @@ describe('enhancer.js', () => {
   });
 
   describe('succeed method', () => {
+    it("should be undefined when argument isn't an object", () => {
+      expect(enhancer.succeed(1)).toBeUndefined();
+    });
+    it('should be defined when argument is an object', () => {
+      expect(enhancer.succeed(item)).toBeDefined();
+    });
+    it('should be undefined when object does not include enhancement', () => {
+      expect(enhancer.succeed({ durability: 10 })).toBeUndefined();
+    });
+    it('should return object with enhancement increased by 1 when less than 20', () => {
+      expect(enhancer.succeed({ enhancement: 19 })).toEqual(
+        expect.objectContaining({
+          enhancement: 20
+        })
+      );
+    })
+    it('should return object with enhancement the same when equal to 20', () => {
+      expect(enhancer.succeed({ enhancement: 20 })).toEqual(
+        expect.objectContaining({
+          enhancement: 20
+        })
+      );
+    })
+  });
 
+  describe('fail method', () => {
+    it("should be undefined when argument isn't an object", () => {
+      expect(enhancer.fail("Hello world")).toBeUndefined();
+    });
+    it('should be defined when argument is an object', () => {
+      expect(enhancer.fail(item)).toBeDefined();
+    });
+    it('should be undefined when object does not include enhancement or durability', () => {
+      expect(enhancer.fail({ name: 'Player 1' })).toBeUndefined();
+    });
+    it('should return durability minus 5 if enhancement is less than 15', () => {
+      expect(enhancer.fail({ enhancement: 14, durability: 5})).toEqual(
+        expect.objectContaining({
+          durability: 0
+        })
+      )
+    });
+    it('should return durability minus 10 if enhancement is equal to 15 or more', () => {
+      expect(enhancer.fail({ enhancement: 16, durability: 20})).toEqual(
+        expect.objectContaining({
+          durability: 10
+        })
+      )
+    });
+    it('should return durability minus 10 and enhancement minus 1 if enhancement is more than 16', () => {
+      expect(enhancer.fail({ enhancement: 17, durability: 20})).toEqual(
+        expect.objectContaining({
+          durability: 10,
+          enhancement: 16
+        })
+      )
+    });
   });
 
 
